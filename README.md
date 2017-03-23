9fcccd32160aec0862fabdfec4bd16bac300dd39
吴晓兰 <wuxiaolan@wecash.net>
2017-03-23 16:11:00 +0800

    更新项目说明文件

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # bugwatch
 
-> 监测bug报错
+> 监测bug报错平台
+## 技术栈：
+- Vue2系列： Vue2 + Vue-router
+- 接口请求： Axios
+- Koa： Koa2， Koa-Router， koa-bodyparser
+- 接口转发配置在 ./config/index 里的 proxyTalbe
+- 本地调试接口请另外开一个控制台 输入 node app.js
+
+-----
+## 项目结构
+- 前端代码在 **./src **
+	src里的目录
+	├─assets
+	│  └─js
+	├─components
+	│  └─dashboard
+	├─config
+	└─router
+- 后端代码现在主要在 **./app.js** ，建模等后端代码放在 **./server** 里
+- 数据库连接放在 **./models/db.js**
+
+
+在本地开发的时候
 
 ## Build Setup
 
--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const Koa = require('koa');
 const app = new Koa();
+const bodyParser = require('koa-bodyparser');
 const router = require('koa-router')();
 
+app.use(router.routes())
+  .use(router.allowedMethods());
+app.use(bodyParser());
 
 router.get('/', function* (next) {
   console.log('我是首页');
   this.body = 'Hello World';
 });
 router.get('/getBugList', function* (next) {
-  console.log('我是api');
-  this.body = 'Hello Api';
+  this.body = this.request;
+  console.dir(this.body);
 });
-app.use(router.routes())
-  .use(router.allowedMethods());
+
 
 app.on('error', (err, ctx) => {
   console.log('Koa服务开启失败', err);
