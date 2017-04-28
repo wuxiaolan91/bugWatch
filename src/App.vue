71c7b88231bd8504ed31b92a41a87f6a2d383a83
吴晓兰 <wuxiaolan@wecash.net>
2017-04-28 18:40:29 +0800

    refacot:左侧导航的修改 author:盛星

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
 <template>
   <div id="app">
     <Top></Top>
-    <div id="nav-wrap">
-      <el-menu default-active="2"
-               class="el-menu-vertical-demo">
+    <div id="nav-wrap" class="left-bar">
+      <el-menu default-active="2" class="el-menu-vertical-demo">
         <el-menu-item index="1">
           <router-link to="/dashboard"><i class="el-icon-message"></i>Dashboard
           </router-link>
@@ -26,7 +25,6 @@
   
   </div>
 </template>
-
 <script>
 import Top from '@/components/common/header.vue';
 export default {
@@ -59,11 +57,74 @@ body {
 
 #wrap {
   position: fixed;
-  left: @navWidth + 10px;
+  left: @navWidth;
   right: 0;
-  top: @top + 10px;
+  bottom: 0;
+  top: @top;
+  padding:20px;
+  overflow: auto;
 }
-
+  .left-bar {
+    text-align: left;
+    padding-top: 0!important;
+    border-right: 1px solid #ddd;
+    position: fixed;
+    width: 220px;
+    top: 40px;
+    left: 0;
+    bottom: 0;
+    padding: 0;
+    z-index: 2;
+    background-color: #f4f5f9;
+    overflow: auto;
+    -webkit-transition: all 0.3s ease 0s;
+    -moz-transition: all 0.3s ease 0s;
+    -ms-transition: all 0.3s ease 0s;
+    -o-transition: all 0.3s ease 0s;
+    transition: all 0.3s ease 0s; }
+  .left-bar ul {
+    padding: 0;
+    margin: 0; }
+  .left-bar ul > li {
+    padding: 0!important;
+    border-bottom: 1px solid #eaeefb; }
+  .left-bar ul > li a {
+    display: block;
+    width: 100%;
+    padding: 15px 20px;
+    color: #1f2f3d;
+    line-height:1em;
+    cursor: pointer;
+    box-sizing: border-box;
+    text-decoration: none; }
+  .left-bar ul > li a .el-icon-arrow-down {
+    float: right;
+    margin-top: 4px;
+    -webkit-transition: all 0.3s ease 0s;
+    -moz-transition: all 0.3s ease 0s;
+    -ms-transition: all 0.3s ease 0s;
+    -o-transition: all 0.3s ease 0s;
+    transition: all 0.3s ease 0s; }
+  .left-bar ul > li a.router-link-active {
+    color: #20a0ff;
+    border-right: 3px solid #20a0ff;
+    background-color: #fff; }
+  .left-bar ul > li .nav-sub {
+    width: 100%;
+    display: none; }
+  .left-bar ul > li .nav-sub > li a {
+    padding-left: 40px; }
+  .left-bar ul > li.expand-submenu .el-icon-arrow-down {
+    -webkit-transform: rotate(180deg);
+    -moz-transform: rotate(180deg);
+    -ms-transform: rotate(180deg);
+    -o-transform: rotate(180deg);
+    transform: rotate(180deg); }
+  .left-bar ul > li.expand-submenu .nav-sub {
+    display: block; }
+  .el-menu-item, .el-submenu__title {
+    height: auto;
+  }
 #nav-wrap {
   display: inline-block;
   position: fixed;
