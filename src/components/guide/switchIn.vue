f212bac975c4f7c09ad8c4fce9a1b8d1b6d48b50
吴晓兰 <wuxiaolan@wecash.net>
2017-05-02 15:42:13 +0800

    feat:添加使用指南文件

---
+++ src/components/guide/switchIn.vue
@@ -0,0 +1,19 @@
+<template>
+<p>
+在每个页面的head标签中，加入以下代码。就可以监听网站的脚本错误了
+<code>
+<script src="/static/js/lib/bugWatch.js"></script>
+    <script>
+      bugWatch.report({
+        projectId: '0f8935a3-789e-46ce-c70f-9553119e3f39',
+        ignore: 'error20'
+      });
+      
+     
+      console.log(error19);
+    </script>
+    </code>
+  </p>
+</atemplate>
+<script>
+</script>
