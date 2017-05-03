e7e833ada59b74f6938d48e8379b1166ee605c21
吴晓兰 <wuxiaolan@wecash.net>
2017-05-03 11:11:40 +0800

    fix:这里引用的js改变。引用的js之前删掉了

--- server/controllers/email.js
+++ server/controllers/email.js
@@ -1,13 +1,18 @@
-const userModel = require('../models/user.js');
+const userModel = require('../models/userModel.js');
 const nodemailer = require("nodemailer");
 const emailAuth = require('../config/local.config.js');
+/**
+ * 发送邮件需要的邮件主机信息和授权信息
+ */
 let transporter = nodemailer.createTransport({
   host: "smtp.163.com", // 主机
   secureConnection: true, // 使用 SSL
   port: 465, // SMTP 端口
   auth: emailAuth.fromEmailAuth
 });
-
+/**
+ * 发送邮件
+ */
 exports.sendemail = function (mailOptions) {
   transporter.sendMail(mailOptions, (error, info) => {
     if(error){
