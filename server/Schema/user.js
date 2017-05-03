fe2490b8c62104c98bde6f95a8be60e445aa6d3b
吴晓兰 <wuxiaolan@wecash.net>
2017-05-03 10:58:30 +0800

    delete:去掉user.js

--- server/Schema/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const user = new mongoose.Schema({
-  name: String, // 用户名
-  email: String, // 邮件
-  pass: String, // 密码
-  role: String //角色
-})
-
-module.exports = user
--- server/models/user.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-const userSchema = require('../Schema/user.js')
-const User = mongoose.model('user', userSchema)
-module.exports = User
