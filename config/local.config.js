31928caf7ea0fca66dbb273173383160ee3dca0d
吴晓兰 <wuxiaolan91@163.com>
2017-05-09 14:44:27 +0800

    fix:去掉连接字符串

--- config/local.config.js
+++ config/local.config.js
@@ -1,10 +1,1 @@
-// 连接mongodb数据库的字符串
-module.exports = {
-  mongo: {
-    DB_URL: 'mongodb://118.184.11.220:27017/?readPreference=primary/bugWatch'
-  },
-  fromEmailAuth: {
-    user: '15600018324@163.com',
-    pass: 'ceshi487073'
-  }
-}
+
