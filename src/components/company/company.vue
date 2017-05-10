00c0e1fdbb8b4333ab73e4091b25ab9fb43d4e4d
ShengXing Chi <ocxers@gmail.com>
2017-05-10 09:04:38 +0800

    Merge branch 'chi' into chi-company
    完成了Company部分功能. 将project以列表的形式展现.

--- src/components/company/company.vue
+++ src/components/company/company.vue
@@ -1,90 +1,78 @@
 <template>
-<div>
-  <el-tabs v-model="activeName" >
-    <el-tab-pane label="公司信息" name="first">
-      
-      <el-card class="box-card"
-        v-loading.body="loading">
-        <div  class="text item">
-          <span>公司名称：</span> {{ companyName }}
-        </div>
-        <div  class="text item">
-          <span>公司id:</span> {{ id }}
-        </div>
-        <div  class="text item">
-          <span>公司拥有者:</span> {{ ownerName }}
-        </div>
-    </el-card>
-    </el-tab-pane>
-    <el-tab-pane label="公司成员" name="second">
-      <!--<el-input placeholder="请输入用户地址"></el-input>
-      <el-button></el-button>-->
-      <el-table
-        :data="userList"
-        stripe
-        style="width: 100%">
-        <el-table-column
-          prop="name"
-          label="用户"
-          >
-        </el-table-column>
-        <el-table-column
-          prop="email"
-          label="邮箱"
-          >
-        </el-table-column>
-      </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="公司项目" name="third">
-      <router-link to="/projectList?type=add">添加项目</router-link><br>
-       {{ projectList }}
-    </el-tab-pane>
-  </el-tabs>
- 
-    
-     
-   
-</div>
+  <div>
+    <el-tabs v-model="activeName">
+      <el-tab-pane label="公司信息" name="first">
+        
+        <el-card class="box-card" v-loading.body="loading">
+          <div class="text item">
+            <span>公司名称：</span> {{ companyName }}
+          </div>
+          <div class="text item">
+            <span>公司id:</span> {{ id }}
+          </div>
+          <div class="text item">
+            <span>公司拥有者:</span> {{ ownerName }}
+          </div>
+        </el-card>
+      </el-tab-pane>
+      <el-tab-pane label="公司成员" name="second">
+        <!--<el-input placeholder="请输入用户地址"></el-input>
+        <el-button></el-button>-->
+        <el-table :data="userList" stripe style="width: 100%">
+          <el-table-column prop="name" label="用户"></el-table-column>
+          <el-table-column prop="email" label="邮箱"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="公司项目" name="third">
+        <router-link to="/projectList?type=add">添加项目</router-link>
+        <!--<br> {{ projectList }}-->
+        <el-table v-loading.body="loading" :data="projectList" stripe style="width: 100%">
+          <el-table-column prop="name" width="150" label="项目名"></el-table-column>
+          <el-table-column prop="_id" label="项目id"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 <script>
-export default {
-  data () {
-    return {
-      activeName: 'first',
-      id: '',
-      ownerId: '',
-      ownerName: '', // 公司的拥有者
-      companyName: '',
-      userList: [],
-      projectList: [],
-      companyId: '3355',
-      loading: false
-    }
-  },created () {
-    
-    this.getCompanyById();
-  }, methods: {
-    getCompanyById () {
-      this.loading =  true;
-      this.$http.get('/api/company/getCompanyById').then(res => {
-        this.loading = false;
-        if (res.data) {
-          let company = res.data;
-          this.id = company._id;
-          this.companyName = company.companyName;
-          this.ownerId = company.ownerId;
-          this.userList = company.userList;
-          this.userList.every(item => {
-            if (item._id = this.ownerId) {
-              this.ownerName = item.name;
-            }
-          })
-          this.projectList = company.projectList;
-        }
-      })
+  export default {
+    data () {
+      return {
+        activeName: 'first',
+        id: '',
+        ownerId: '',
+        ownerName: '', // 公司的拥有者
+        companyName: '',
+        userList: [],
+        projectList: [],
+        companyId: '3355',
+        loading: false
+      }
+    }, created () {
+      
+      this.getCompanyById();
+    }, methods: {
+      getCompanyById () {
+        this.loading = true;
+        this.$http.get('/api/company/getCompanyById').then(res => {
+          this.loading = false;
+          if (res.data) {
+            let company = res.data;
+            this.id = company._id;
+            this.companyName = company.companyName;
+            this.ownerId = company.ownerId;
+            this.userList = company.userList;
+            this.userList.every(item => {
+              if (item._id = this.ownerId) {
+                this.ownerName = item.name;
+              }
+            })
+            this.projectList = company.projectList;
+          }
+        })
+      }
     }
   }
-}
 </script>
 <style lang="less" scoped>
   .item {
