d81f6c55d2d44f18aaaf392acd0995d1c8f712b4
吴晓兰 <wuxiaolan@wecash.net>
2017-05-31 16:54:17 +0800

    optimize:增加ajax报错的几个字段是必填的

--- server/models/ajaxModel.js
+++ server/models/ajaxModel.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ajaxSchema = new Schema({
-  projectId: String,
-  errorPage: String,
+  projectId: {
+    type: String,
+    required: true
+  },
+  errorPage:  {
+    type: String,
+    required: true
+  },
   url: String,
   message: String,
   error: String,
