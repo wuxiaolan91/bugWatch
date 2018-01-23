78800297262d591ea9184fb45395b760e550d7ac
wuxiaolan91 <wuxiaolan91@163.com>
2018-01-23 16:22:12 +0800

    fix:robo3t 连接数据库会报错的 bug

--- server/app.js
+++ server/app.js
@@ -1,23 +1,27 @@
 const app = require('./router/index')
 let config = require('./config')
 const mongoose = require('mongoose')
+mongoose.Promise = global.Promise;
 app.on('error', (err, ctx) => {
   console.log('Koa服务开启失败', err);
 });
 
 app.listen(config.port, () => {
-  console.log('Koa 服务开启');
+  console.log('--------Koa 服务开启--------');
 });
 
 /**
  * 连接
  */
-mongoose.connect(config.mongo.DB_URL);
+mongoose.connect(config.mongo.DB_URL, {useMongoClient: true});
 
 /**
  * 连接成功
  */
 mongoose.connection.on('connected', () => {
   console.log('数据库连接成功');
+});
+mongoose.connection.on('error', function (err) {
+  console.log('-----Mongoose connection error: ' + err);
 });
 
