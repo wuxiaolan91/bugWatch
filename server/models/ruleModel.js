c242615791d57d568e6ac906d5a2a01a71449ac5
wuxiaolan91 <wuxiaolan91@163.com>
2018-01-27 18:46:54 +0800

    rule里有几个字段是必须的

--- server/models/ruleModel.js
+++ server/models/ruleModel.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ruleSchema = new Schema({
-  projectId: String,
-  email: String,
-  keyword: String,
+  projectId: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  keyword: {
+    type: String,
+    required: true
+  },
 });
 const ruleModel = mongoose.model('Rule', ruleSchema);
 module.exports = ruleModel;
